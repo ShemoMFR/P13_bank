0e20be7f7225e544cbbcd78946bb74be49c2d103
Shemo <moshe.frydmann@gmail.com>
2022-01-19 14:03:14 +0200

    navbar ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 /* Pages */
 import Home from './pages/Home/Home';
 
+/* Components */
+import NavBar from './components/NavBar/NavBar';
+
 /* CSS */
 import './App.css';
 
 function App() {
     return (
         <div className="App">
+            <NavBar />
             <Home />
         </div>
     );
